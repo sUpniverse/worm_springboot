9c4547a6ce41545eaa222de46f4af5d90b3e401d
김문섭 <kmsup2@gmail.com>
2018-05-15 21:58:10 +0900

    first spring boot

---
+++ my-sup2/src/main/java/net/sup2/MySup2Application.java
@@ -0,0 +1,12 @@
+package net.sup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySup2Application.class, args);
+	}
+}
---
+++ my-sup2/src/test/java/net/sup2/MySup2ApplicationTests.java
@@ -0,0 +1,16 @@
+package net.sup2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySup2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
