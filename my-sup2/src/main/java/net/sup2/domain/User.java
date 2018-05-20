e5af36dd512077b970fac2302bcdd0421d86f434
김문섭 <kmsup2@gmail.com>
2018-05-21 00:31:15 +0900

    h2 db 및 회원가입

---
+++ my-sup2/src/main/java/net/sup2/domain/User.java
@@ -0,0 +1,61 @@
+package net.sup2.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable=false, length=20)
+	private String userId;
+	private String email;
+	private String name;
+	private String password;
+	
+	
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", email=" + email + ", name=" + name + ", password=" + password + "]";
+	}	
+	
+
+}
--- my-sup2/src/main/java/net/sup2/web/UserController.java
+++ my-sup2/src/main/java/net/sup2/web/UserController.java
@@ -3,10 +3,14 @@ package net.sup2.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import net.sup2.domain.User;
+import net.sup2.domain.UserRepository;
 
 
 @Controller
@@ -14,16 +18,21 @@ public class UserController {
 	
 	List<User> users = new ArrayList<>();
 	
+	@Autowired
+	private UserRepository userRepository;
+		
+	
 	@PostMapping("/create")
 	public String create(User user) {
-		System.out.println("user : " + user);
-		users.add(user);
-		return "redirect:list";
+		System.out.println("user :" + user);
+		userRepository.save(user);
+		return "redirect:/list";
 	}
 	
 	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users",users);
-		return "list";
+		model.addAttribute("users",userRepository.findAll());
+		return "user/list";
 	}
 }
+
